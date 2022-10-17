40e04bfb04fb8acada57012d29f087b69eefc419
Dominik Boronjek <dominikb.hr@gmail.com>
2022-10-17 22:52:56 +0200

    s

---
+++ LV1/ZD1/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+    System.out.println("   J    a   V     V  a ");
+    System.out.println("   J   a a   V   V  a a ");
+    System.out.println("J  J  aaaaa   V V  aaaaa ");
+    System.out.println(" JJ  a     a   V  a     a ");
+        
+        
+        
+    }
+}
