bec765e4dc0d9b45ee5a3ffeb083e01ad1a63cb6
Dominik Boronjek <dominikb.hr@gmail.com>
2022-10-29 09:17:49 +0200

    Update App.java

--- LV1/ZD1/src/App.java
+++ LV1/ZD1/src/App.java
@@ -1,10 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
        
-    System.out.println("   J    a   V     V  a ");
-    System.out.println("   J   a a   V   V  a a ");
-    System.out.println("J  J  aaaaa   V V  aaaaa ");
-    System.out.println(" JJ  a     a   V  a     a ");
+    System.out.println("   J    a   V     V  a \n   J   a a   V   V  a a ");
+    System.out.println("J  J  aaaaa   V V  aaaaa \n JJ  a     a   V  a     a ");
         
         
         
