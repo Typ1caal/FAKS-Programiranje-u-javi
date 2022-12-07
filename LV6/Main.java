9f81b5f69becca45356035a72fe3edb5325e688e
Dominik Boronjek <dominikb.hr@gmail.com>
2022-12-07 17:25:18 +0100

    new

---
+++ LV6/Main.java
@@ -0,0 +1,13 @@
+import javax.print.event.PrintJobListener;
+
+import java.lang.*;
+
+public class Main{
+    
+    public static void main(String[] args) {
+        
+        System.out.println("da");
+    }
+
+}
+
